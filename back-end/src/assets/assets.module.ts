ad251864379272e0d4c3ad1d84748efae1677603
ImGabreuw <imgabreuw.dev@gmail.com>
2023-06-21 15:21:03 -0300

    feat: create home broker back-end

---
+++ back-end/src/assets/assets.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { AssetsService } from './assets.service';
+import { AssetsController } from './assets.controller';
+
+@Module({
+  controllers: [AssetsController],
+  providers: [AssetsService],
+})
+export class AssetsModule {}
